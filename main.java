540ebb1d72b3b5a19fe3e9de67498ec282c5119d
Kavin V <kavin.v@grootan.com>
2026-05-08 17:32:30 +0530

    feat(sum): added sum function

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 class Main{
     public static void main(String[] args){
         System.out.print("Hello world! from main" );
+        System.out.print(sum(1, 2));
+    }
+
+    public static int sum(int num1 , int num2){
+        return num1 + num2;
     }
 }

868400f0d2a58e5b93347977818bf54f8881942e
Kavin V <kavin.v@grootan.com>
2026-05-08 17:41:37 +0530

    feat(sum): added multiply function

--- main.java
+++ main.java
@@ -2,9 +2,15 @@ class Main{
     public static void main(String[] args){
         System.out.print("Hello world! from main" );
         System.out.print(sum(1, 2));
+        System.out.print(multiply(2,256));
     }
 
     public static int sum(int num1 , int num2){
         return num1 + num2;
+    }
+
+
+    public static int multiply(int num1 , int num2){
+        return num1 * num2;
     }
 }
